82950000728cf7d0d8faa1d20dde0b7ca0e84480
Kevin Santiago Castaño <santiagocastano47@gmail.com>
2024-04-10 22:51:57 -0500

    Pay Mode Presenter

--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a0d0a19633ae491b39970d4877985eeb29a0f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc81bd693c832d06da417382da7f0f5697ade829")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
