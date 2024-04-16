98d4f25fc000a2b42cbf5d7717927000e1181be7
Kevin Santiago Castaño <santiagocastano47@gmail.com>
2024-04-15 21:58:06 -0500

    Product View

--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab2b3a6bd7f3d1c9727b89e301e63f20561afb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb806a5f2049624c399132eac95780d460deb8d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
